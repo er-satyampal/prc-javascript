3107dbda0bc4edf52075c8455e350bd8577c9912
Satyam Pal <em.satyam@gmail.com>
2022-07-27 22:36:23 +0530

    for_loop

---
+++ forloop/for_loop.js
@@ -0,0 +1,6 @@
+let days =["Sunday", "Monday","Tuesday","Wednesday","Thursday", "Friday", "Saturday"];
+let text = "";
+let v = 0;
+for (v; v < days.length; v++){
+  console.log(days[v]);
+}              
