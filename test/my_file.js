e9d08981da40e2fc42936072d7cb67d8d5b78c48
unknown <em.satyam@gmail.com>
2022-08-16 12:38:27 +0530

    Some File-Changes

--- test/my_file.js
+++ test/my_file.js
@@ -13,6 +13,8 @@ document.getElementById("processForm").addEventListener("click", function (e) {
         document.getElementById("fname").addEventListener("keyup", function () {
             document.getElementById("msg").innerHTML = "";
         });
+
+    return false;
     }
 
 
@@ -22,21 +24,16 @@ document.getElementById("processForm").addEventListener("click", function (e) {
         document.getElementById("lname").addEventListener("keyup", function () {
             document.getElementById("msg").innerHTML = "";
         });
+
+        return false;
     }
+
     if ((state.length == " ")||(state.length >= 16)){
-        document.getElementById("msg").innerHTML = "Condition for last name does not met";
+        document.getElementById("msg").innerHTML = "Condition for state does not met";
         document.getElementById("state").addEventListener("keyup", function(){
             document.getElementById("msg").innerHTML ="";
         });
+
+        return false;
     }
 });
-
-
-/*
-if ((last_name.length == "") || (last_name.length >= 13)) {
-    document.getElementById("msg").innerHTML = "Condition for last name does not met";
-    document.getElementById("lname").addEventListener(keyup, function ()){
-        document.getElementById("msg").innerHTML = "";
-    }
-};
-*/
