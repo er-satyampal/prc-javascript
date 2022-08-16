fed2bcb47511ab4cf581370aa8d1849fb3601c9f
unknown <em.satyam@gmail.com>
2022-08-16 12:08:55 +0530

    Login-form

--- test/my_file.js
+++ test/my_file.js
@@ -1,21 +1,42 @@
 
-document.getElementById("processForm").addEventListener("click", function(e){
+document.getElementById("processForm").addEventListener("click", function (e) {
     e.preventDefault();
 
-    let first_name =  document.getElementById("fname").value;
-    let last_name =  document.getElementById("lname").value;
-    let state =  document.getElementById("state").value;
+    let first_name = document.getElementById("fname").value;
+    let last_name = document.getElementById("lname").value;
+    let state = document.getElementById("state").value;
 
 
-    if ((first_name.length == "") || (first_name.length >= 13 )){
+    if ((first_name.length == "") || (first_name.length >= 13)) {
         document.getElementById("msg").innerHTML = "Condition for first name does not met";
 
-        document.getElementById("fname").addEventListener("keyup", function(){
+        document.getElementById("fname").addEventListener("keyup", function () {
             document.getElementById("msg").innerHTML = "";
         });
     }
 
 
-  });  
 
-  document.getElementById("process")
+    if ((last_name.length == "") || (last_name.length >= 10)) {
+        document.getElementById("msg").innerHTML = "Condition for last name does not met";
+        document.getElementById("lname").addEventListener("keyup", function () {
+            document.getElementById("msg").innerHTML = "";
+        });
+    }
+    if ((state.length == " ")||(state.length >= 16)){
+        document.getElementById("msg").innerHTML = "Condition for last name does not met";
+        document.getElementById("state").addEventListener("keyup", function(){
+            document.getElementById("msg").innerHTML ="";
+        });
+    }
+});
+
+
+/*
+if ((last_name.length == "") || (last_name.length >= 13)) {
+    document.getElementById("msg").innerHTML = "Condition for last name does not met";
+    document.getElementById("lname").addEventListener(keyup, function ()){
+        document.getElementById("msg").innerHTML = "";
+    }
+};
+*/
